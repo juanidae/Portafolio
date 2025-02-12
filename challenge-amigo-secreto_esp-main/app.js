049580290734635ab9a99c576c398caa88cec7ae
Juan Gonzalez <juan.gonzalez@idaesoluciones.com>
2025-02-12 00:01:13 -0500

    creacion de funcion,agregar nombres a la lista

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombres = [];
+
+function agregarAmigo() {
+  let nombre = String(document.getElementById("amigo").value.trim());  // Obtiene el valor del input y lo convierte a string
+  if (nombre === "" || !isNaN(nombre) || nombre.length < 4 || nombre.match(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/)) {  // Verifica si está vacío o si es un número
+    alert("Por favor, ingrese un nombre válido.");
+  } else {
+    nombres.push(nombre);
+    document.getElementById("amigo").value = "";
+  }
+  console.log(nombres);
+  console.log(nombres.length);
+}
+
+
